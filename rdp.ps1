f6ee19b16152a5a202dae9f19991a87f011bb81f
aadupoom1 <85998797+aadupoom1@users.noreply.github.com>
2021-07-23 20:10:55 +0300

    Update rdp.ps1

--- rdp.ps1
+++ rdp.ps1
@@ -4,7 +4,7 @@ Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd6
 unzip ngrok.zip
 Copy ngrok.exe C:\Windows\System32
 Start-Service -Name audiosrv
-cmd /c echo ./ngrok.exe authtoken "1uYqYSLJQDBJtf1cUhDTCiED1Sh_4MLAgNXZS5ntBWD4EjzUa" >a.ps1
+cmd /c echo ./ngrok.exe authtoken "1vAd7rVcLgACU6bpo0yLy765G2F_3x49TVb8eggTi6r9RHPhk" >a.ps1
 cmd /c echo cmd /k start ngrok.exe tcp 3389 >>a.ps1
 cmd /c echo ping -n 999999 10.10.10.10 >>a.ps1
  Write-Host "Visit: https://dashboard.ngrok.com/endpoints/status to get NGROK IP"
